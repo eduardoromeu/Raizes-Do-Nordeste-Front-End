247c1a3f8fc4e65175f200902d06b3074072d399
Dudu <eduardoromeueberts@gmail.com>
2026-07-18 06:23:00 -0300

    atualizado navbar e feito esboço do carregamento de páginas

--- js/components.js
+++ js/components.js
@@ -1,6 +1,30 @@
+// Carrega um componente html dentro de um container
 async function loadComponent(id, path) {
   const response = await fetch(path);
   const html = await response.text();
 
   document.getElementById(id).innerHTML = html;
 }
+
+async function loadPage(id, page) {
+  const response = await fetch(`../pages/${page}.html`);
+  const html = await response.text();
+
+  const container = document.getElementById(id);
+
+  const temp = document.createElement("div");
+  temp.innerHTML = html;
+
+  const firstChild = temp.firstElementChild;
+
+  if (firstChild) {
+    container.replaceChildren(...firstChild.children);
+  }
+}
+
+// async function loadPage(id, page) {
+//   const response = await fetch(`../pages/${page}.html`);
+//   const html = await response.text();
+
+//   document.getElementById(id).innerHTML = html;
+// }
